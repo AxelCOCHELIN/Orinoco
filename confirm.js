59aa1f3cc60e3f6ae35cc92eed5e67ef9b59f619
Axel COCHELIN <ax.cochelin@gmail.com>
2020-07-14 21:36:20 +0200

    Ajout nom et recap pour la commande

--- confirm.js
+++ confirm.js
@@ -10,7 +10,23 @@ let orderId = orderInfo.orderId;
 totalPriceDisplay.textContent = totalPrice; //affichage du prix total
 orderIdDisplay.textContent = orderId; //affichage de l'id de commande
 
+//récapitulatif de la commande
+let recapDisplay = document.querySelector(".recap p");
+let nameDisplay = document.querySelector(".name");
+let recapOrder = localStorage.getItem("productsToBuy");
+let nameGet = localStorage.getItem("orderInfos");
+recapOrder = JSON.parse(recapOrder);
+nameGet = JSON.parse(nameGet);
+nameDisplay.textContent = `${nameGet.contact.firstName} ${nameGet.contact.lastName}`;
+for (const BuyedBear of recapOrder) {
+  recapDisplay.textContent += `
+Un adorable ourson nommé ${BuyedBear.name} dont la couleur est ${
+    BuyedBear.color
+  } et dont le prix unitaire est de ${BuyedBear.price / 100},00 €.
+`;
+}
+
 //reset le panier au départ de la page
-window.addEventListener("unload", function () {
+/*window.addEventListener("unload", function () {
   localStorage.clear();
-});
+});*/
